10117ae019a25d2a680a2342e7743af89625b21f
santa <ryancurtis998@yahoo.com>
2019-04-29 12:53:40 +0300

    additio of comment test

--- tests/comment_test.py
+++ tests/comment_test.py
@@ -1,18 +1,16 @@
 import unittest
-
-from app.models import User, BLOG
+from app.models import Comment,User
 from app import db
 
-class BloghModelTest(unittest.TestCase):
+class CommentModelTest(unittest.TestCase):
     def setUp(self):
         self.user_francis = User(username = 'Francis',password = 'Password')
-        self.new_blog = BLOG(m_blog_title='Test',m_blog_content='Test',m_blog_posted_on='2019-02-18',m_user_id = '1')
+        self.new_comment = Comment(c_content='test', c_blog_id ='1')
 
     def test_check_instance_variable(self):
-        self.assertEquals(self.new_blog.m_blog_title,'Test')
-        self.assertEquals(self.new_blog.m_blog_content,'Test')
-        self.assertEquals(self.new_blog.m_blog_posted_on,'2019-02-18')
-        self.assertEquals(self.new_blog.m_user_id, '1')
+        self.assertEquals(self.new_comment.c_content,'test')
+        self.assertEquals(self.new_comment.c_blog_id,'1')
 
-    def test_save_blog(self):
-        self.new_blog.save_blog()
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all())>0)
