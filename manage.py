9d3546c4fcf48cbdd696d5becd71e21cde1b81af
santa <ryancurtis998@yahoo.com>
2019-04-29 11:53:47 +0300

    ammend

--- manage.py
+++ manage.py
@@ -1,18 +1,18 @@
 from app import create_app, db
 from flask_script import Manager,Server
-# from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Blog
 
 # instances for the create_app
-#app = create_app('test')
+# app = create_app('test')
 app = create_app('development')
-#app = create_app('production')
+# app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
